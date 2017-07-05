9c776693300b9ae4ebae63fd11c46d73b29863f3
Rashi Sharma <rashi141096@gmail.com>
2017-07-05 10:43:32 +0530

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -6,58 +6,72 @@ import psycopg2
 DBNAME = "news"
 
 
-def top_articles():
+def connect(dbname="news"):
     """
-    Prints top 3 most accessed articles with number of views as sorted list
-    with most popular article at the top.
+    Returns the db and cursor variables.
     """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("""select title, num from articles left join tops on tops.path
-              like '%'||(articles.slug) order by num desc limit 3""")
+    try:
+        db = psycopg2.connect("dbname=news".format(DBNAME))
+        c = db.cursor()
+        return db, c
+    except psycopg2.Error as e:
+        print "Unable to connect to database"
+        sys.exit(1)
+
+
+def fetch_query(query):
+    """
+    Connect to the database, query, fetch results, close, return results.
+    """
+    db, c = connect()
+    c.execute(query)
     results = c.fetchall()
+    db.close()
+    return results
+
+
+def print_top_articles():
+    """
+    Fetch top articles using helper function, print results, close connection.
+    """
+    results = fetch_query(
+	      """select title, num from articles left join tops on tops.path
+              like '%'||(articles.slug) order by num desc limit 3""")
     print "\nMost popular three articles of all time :\n"
     for part in results:
         print "\t- \"", part[0], "\" -->", part[1], "views"
-    db.close()
 
 
-def top_authors():
+def print_top_authors():
     """
-    Prints a sorted list of authors and number of views with the most popular
-    author at the top
+    Fetch top authors using helper function, print results, close connection.
     """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("""select name, sum(num) as views from (articles join tops on
+    results = fetch_query(
+              """select name, sum(num) as views from (articles join tops on
               tops.path like '%'||(articles.slug)) as arti left join authors
               on authors.id = arti.author group by name order by views desc""")
-    results = c.fetchall()
     print "\nMost popular article authors of all time :\n"
     for part in results:
         print "\t-", part[0], "-->", part[1], "views"
-    db.close()
 
 
-def log_errors():
+def print_top_error_days():
     """
-    Prints the date and error percentage of days when more than 1% of requests
-    lead to errors.
+    Fetch top error days using helper function, print results, close
+    connection.
     """
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("""select to_char(cast(time as date), 'Month DD, YYYY'),
+    results = fetch_query(
+	      """select to_char(cast(time as date), 'Month DD, YYYY'),
               round((count(case status when '404 NOT FOUND' then 1 else null
               end)*100.0)/count(status),2) as percent from log group by
               cast(time as date) having ((count(case status when
               '404 NOT FOUND' then 1 else null end)*100.0)/count(status))>1""")
-    results = c.fetchall()
     print "\nDays when more than 1% of requests lead to errors :\n"
     for part in results:
         print "\t-", part[0], "-->", part[1], "% errors"
-    db.close()
 
 
-top_articles()
-top_authors()
-log_errors()
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_top_error_days()
